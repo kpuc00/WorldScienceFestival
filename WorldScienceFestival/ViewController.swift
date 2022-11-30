88d3b18e8725c716d55407919b040ea38151da8c
Kristiyan Strahilov <55084258+kpuc00@users.noreply.github.com>
2022-11-30 15:46:35 +0100

    Initial Commit

---
+++ WorldScienceFestival/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldScienceFestival
+//
+//  Created by Kristiyan Strahilov on 30.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
